0cffb173636cb3791f5d12bfedb4ec64bf97a451
JaeHun Kim <kkaa81@naver.com>
2022-01-15 18:31:04 +0900

    feat: fake data

---
+++ backend/json.js
@@ -0,0 +1,529 @@
+export const traits = {
+    "championTrait": [
+      {
+        "trait": "1",
+        "champion": "58"
+      },
+      {
+        "trait": "1",
+        "champion": "37"
+      },
+      {
+        "trait": "1",
+        "champion": "78"
+      },
+      {
+        "trait": "2",
+        "champion": "58"
+      },
+      {
+        "trait": "2",
+        "champion": "26"
+      },
+      {
+        "trait": "2",
+        "champion": "35"
+      },
+      {
+        "trait": "2",
+        "champion": "33"
+      },
+      {
+        "trait": "2",
+        "champion": "23"
+      },
+      {
+        "trait": "2",
+        "champion": "32"
+      },
+      {
+        "trait": "3",
+        "champion": "50"
+      },
+      {
+        "trait": "3",
+        "champion": "29"
+      },
+      {
+        "trait": "3",
+        "champion": "69"
+      },
+      {
+        "trait": "3",
+        "champion": "52"
+      },
+      {
+        "trait": "3",
+        "champion": "58"
+      },
+      {
+        "trait": "3",
+        "champion": "22"
+      },
+      {
+        "trait": "3",
+        "champion": "68"
+      },
+      {
+        "trait": "4",
+        "champion": "59"
+      },
+      {
+        "trait": "4",
+        "champion": "56"
+      },
+      {
+        "trait": "4",
+        "champion": "45"
+      },
+      {
+        "trait": "4",
+        "champion": "55"
+      },
+      {
+        "trait": "5",
+        "champion": "19"
+      },
+      {
+        "trait": "5",
+        "champion": "20"
+      },
+      {
+        "trait": "5",
+        "champion": "23"
+      },
+      {
+        "trait": "5",
+        "champion": "25"
+      },
+      {
+        "trait": "5",
+        "champion": "46"
+      },
+      {
+        "trait": "5",
+        "champion": "48"
+      },
+      {
+        "trait": "5",
+        "champion": "62"
+      },
+      {
+        "trait": "6",
+        "champion": "59"
+      },
+      {
+        "trait": "6",
+        "champion": "47"
+      },
+      {
+        "trait": "6",
+        "champion": "65"
+      },
+      {
+        "trait": "6",
+        "champion": "36"
+      },
+      {
+        "trait": "6",
+        "champion": "46"
+      },
+      {
+        "trait": "6",
+        "champion": "73"
+      },
+      {
+        "trait": "6",
+        "champion": "61"
+      },
+      {
+        "trait": "7",
+        "champion": "47"
+      },
+      {
+        "trait": "7",
+        "champion": "65"
+      },
+      {
+        "trait": "7",
+        "champion": "36"
+      },
+      {
+        "trait": "7",
+        "champion": "46"
+      },
+      {
+        "trait": "7",
+        "champion": "73"
+      },
+      {
+        "trait": "7",
+        "champion": "61"
+      },
+      {
+        "trait": "8",
+        "champion": "48"
+      },
+      {
+        "trait": "9",
+        "champion": "42"
+      },
+      {
+        "trait": "9",
+        "champion": "72"
+      },
+      {
+        "trait": "9",
+        "champion": "47"
+      },
+      {
+        "trait": "9",
+        "champion": "27"
+      },
+      {
+        "trait": "9",
+        "champion": "52"
+      },
+      {
+        "trait": "9",
+        "champion": "22"
+      },
+      {
+        "trait": "9",
+        "champion": "40"
+      },
+      {
+        "trait": "9",
+        "champion": "34"
+      },
+      {
+        "trait": "10",
+        "champion": "54"
+      },
+      {
+        "trait": "10",
+        "champion": "71"
+      },
+      {
+        "trait": "10",
+        "champion": "41"
+      },
+      {
+        "trait": "10",
+        "champion": "21"
+      },
+      {
+        "trait": "10",
+        "champion": "31"
+      },
+      {
+        "trait": "10",
+        "champion": "25"
+      },
+      {
+        "trait": "10",
+        "champion": "34"
+      },
+      {
+        "trait": "11",
+        "champion": "28"
+      },
+      {
+        "trait": "11",
+        "champion": "66"
+      },
+      {
+        "trait": "11",
+        "champion": "45"
+      },
+      {
+        "trait": "11",
+        "champion": "53"
+      },
+      {
+        "trait": "12",
+        "champion": "63"
+      },
+      {
+        "trait": "12",
+        "champion": "29"
+      },
+      {
+        "trait": "12",
+        "champion": "73"
+      },
+      {
+        "trait": "12",
+        "champion": "77"
+      },
+      {
+        "trait": "13",
+        "champion": "68"
+      },
+      {
+        "trait": "14",
+        "champion": "41"
+      },
+      {
+        "trait": "14",
+        "champion": "67"
+      },
+      {
+        "trait": "14",
+        "champion": "36"
+      },
+      {
+        "trait": "14",
+        "champion": "37"
+      },
+      {
+        "trait": "15",
+        "champion": "42"
+      },
+      {
+        "trait": "15",
+        "champion": "49"
+      },
+      {
+        "trait": "15",
+        "champion": "56"
+      },
+      {
+        "trait": "15",
+        "champion": "74"
+      },
+      {
+        "trait": "15",
+        "champion": "39"
+      },
+      {
+        "trait": "15",
+        "champion": "77"
+      },
+      {
+        "trait": "16",
+        "champion": "50"
+      },
+      {
+        "trait": "16",
+        "champion": "65"
+      },
+      {
+        "trait": "16",
+        "champion": "76"
+      },
+      {
+        "trait": "16",
+        "champion": "24"
+      },
+      {
+        "trait": "16",
+        "champion": "68"
+      },
+      {
+        "trait": "17",
+        "champion": "60"
+      },
+      {
+        "trait": "17",
+        "champion": "64"
+      },
+      {
+        "trait": "17",
+        "champion": "21"
+      },
+      {
+        "trait": "17",
+        "champion": "58"
+      },
+      {
+        "trait": "17",
+        "champion": "22"
+      },
+      {
+        "trait": "17",
+        "champion": "61"
+      },
+      {
+        "trait": "18",
+        "champion": "19"
+      },
+      {
+        "trait": "18",
+        "champion": "60"
+      },
+      {
+        "trait": "18",
+        "champion": "33"
+      },
+      {
+        "trait": "18",
+        "champion": "37"
+      },
+      {
+        "trait": "19",
+        "champion": "49"
+      },
+      {
+        "trait": "19",
+        "champion": "54"
+      },
+      {
+        "trait": "19",
+        "champion": "33"
+      },
+      {
+        "trait": "19",
+        "champion": "69"
+      },
+      {
+        "trait": "19",
+        "champion": "44"
+      },
+      {
+        "trait": "19",
+        "champion": "53"
+      },
+      {
+        "trait": "19",
+        "champion": "57"
+      },
+      {
+        "trait": "20",
+        "champion": "51"
+      },
+      {
+        "trait": "20",
+        "champion": "27"
+      },
+      {
+        "trait": "20",
+        "champion": "74"
+      },
+      {
+        "trait": "20",
+        "champion": "53"
+      },
+      {
+        "trait": "20",
+        "champion": "48"
+      },
+      {
+        "trait": "21",
+        "champion": "29"
+      },
+      {
+        "trait": "21",
+        "champion": "57"
+      },
+      {
+        "trait": "22",
+        "champion": "63"
+      },
+      {
+        "trait": "22",
+        "champion": "64"
+      },
+      {
+        "trait": "22",
+        "champion": "70"
+      },
+      {
+        "trait": "22",
+        "champion": "24"
+      },
+      {
+        "trait": "22",
+        "champion": "55"
+      },
+      {
+        "trait": "23",
+        "champion": "66"
+      },
+      {
+        "trait": "23",
+        "champion": "39"
+      },
+      {
+        "trait": "23",
+        "champion": "78"
+      },
+      {
+        "trait": "24",
+        "champion": "26"
+      },
+      {
+        "trait": "24",
+        "champion": "71"
+      },
+      {
+        "trait": "24",
+        "champion": "51"
+      },
+      {
+        "trait": "24",
+        "champion": "38"
+      },
+      {
+        "trait": "24",
+        "champion": "32"
+      },
+      {
+        "trait": "24",
+        "champion": "43"
+      },
+      {
+        "trait": "25",
+        "champion": "77"
+      },
+      {
+        "trait": "26",
+        "champion": "20"
+      },
+      {
+        "trait": "26",
+        "champion": "64"
+      },
+      {
+        "trait": "26",
+        "champion": "76"
+      },
+      {
+        "trait": "26",
+        "champion": "40"
+      },
+      {
+        "trait": "26",
+        "champion": "57"
+      },
+      {
+        "trait": "27",
+        "champion": "35"
+      },
+      {
+        "trait": "27",
+        "champion": "54"
+      },
+      {
+        "trait": "27",
+        "champion": "28"
+      },
+      {
+        "trait": "27",
+        "champion": "70"
+      },
+      {
+        "trait": "27",
+        "champion": "31"
+      },
+      {
+        "trait": "27",
+        "champion": "74"
+      },
+      {
+        "trait": "28",
+        "champion": "30"
+      }
+    ]
+  }
+  
