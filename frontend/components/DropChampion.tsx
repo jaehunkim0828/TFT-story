85b4bb80f823488bc8902825f5f10670ce9ca72e
JaeHun Kim <kkaa81@naver.com>
2022-01-20 22:53:00 +0900

    change value

--- frontend/components/DropChampion.tsx
+++ frontend/components/DropChampion.tsx
@@ -13,16 +13,18 @@ import { countUpChamp } from '../store/actions/numberOfChampAct';
 export default function DropChampion({ styles, indexs }: { styles: object, indexs: number }) {
     const dispatch = useDispatch();
     const { image, id } = useSelector((state: RootState) => state.championImgReducer);
-    const { member } = useSelector((state: RootState) => state.numberOfChampReducer);
     const [isImage, setIsImage] = useState(false);
     const [boxImg, setBoxImg] = useState('');
 
     const getchampionTraits = async () => {
-        return axios.get(`http://localhost:8888/champion/${id}`)
-        .then(({ data }) => data.map((trait: string) => {
-            console.log(trait);
-            dispatch(countUp(trait))
-        }));
+        return await axios.get(`http://localhost:8888/champion/${id}`)
+        .then(({ data }) => {
+            console.log(data);
+            data.map((trait: string) => {
+                console.log(trait);
+                dispatch(countUp(trait))
+            })
+        });
     }
 
     const [{ isOver }, drop] = useDrop(() => ({
