6669b5e6947171f4ed0b61f81c4cd74e9b09c339
JaeHun Kim <kkaa81@naver.com>
2022-01-20 22:53:32 +0900

    fix(function): object to array

--- frontend/components/TraitCount.tsx
+++ frontend/components/TraitCount.tsx
@@ -5,19 +5,28 @@ import style from '../styles/traitCount.module.scss';
 import Trait from './Trait';
 import { RootState } from '../store/reducers';
 export default function TraitCount() {
-    const [traits, setTraits] = useState([{ count: 0, name: '고물상' }]);
+    const [traits, setTraits] = useState([{ 'name': '고물상', 'count': 0 }]);
 
+    const { member } = useSelector((state: RootState) => state.numberOfChampReducer);
     const { trait } = useSelector((state: RootState) => state.traitReducer);
 
+    const traitToArray = (trait: { [key in string]: number }) => {
+        const keys = Object.keys(trait);
+        const values = Object.values(trait);
+        return keys.map((name, i) => { 
+            return {'name': name, 'count':  values[i]};
+        });
+    }
+
     useEffect(() => {
-        console.log('test');
-    }, [])
+        setTraits(traitToArray(trait));
+    }, [member]);
 
     return (
         <div className={style.container}>
-            {traits.map(trait => {
+            {traits.map((trait, i) => {
                 if (trait.count > 0) {
-                    return <Trait trait={trait}/>
+                    return <Trait key={i} trait={trait}/>
                 }
             })}
         </div>
