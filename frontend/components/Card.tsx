d5b2c22d3f3eaccc35ce593067bb7bc3723d8ae4
JaeHun Kim <kkaa81@naver.com>
2022-01-24 00:52:10 +0900

    feat(card): created card component

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -1,26 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 import style from '../styles/cardList.module.scss';
 
+import { CardItem} from '../type';
 
-interface Card {
-    key: number,
-    deck: { 
-        id: number, 
-        trait: string[], 
-        name: string, 
-        image: string, 
-        description: string
-    }
-}
-
-export default function Card({key, deck}: Card) {
+export default function Card({key, deck}: CardItem) {
     return (
         <div className={style.card} key={key}>
             <img className={style.cardImage}src={deck.image} alt='deck-image'/>
             <div>
                 <div className={style.name}>{deck.name}</div>
                 <div className={style.trait}>
-                    {deck.trait.map((item, i) => <div key={i}>{item}</div>)}
+                    {deck.trait.split(' , ').map((item, i) => <div key={i}>#{item}</div>)}
                 </div>
                 <div>설명: {deck.description}</div>
             </div>
