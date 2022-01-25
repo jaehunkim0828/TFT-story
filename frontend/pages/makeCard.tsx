fd3b4b2e14ed939795a6081d7faace24cb9b41e4
JaeHun Kim <kkaa81@naver.com>
2022-01-25 23:09:16 +0900

    feat(page): create makecard page

--- frontend/pages/makeCard.tsx
+++ frontend/pages/makeCard.tsx
@@ -1,9 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import DeckInfo from "../components/DeckInfo";
 import FinalDeck from "../components/FinalDeck";
 
-export default function makeCard() {
+export default function MakeCard() {
+    const [champions, setChampion] = useState<object[]>([]);
+
+
+    const getAll = async () => {
+        await axios.get('http://localhost:8888/champion')
+        .then((champs: any) => {
+            setChampion(champs.data)
+        });
+    }
+
+    useEffect(() => {
+        getAll();
+    }, [])
     return (
-        <>
-            <FinalDeck />
-        </>
+        <div>
+            <DeckInfo champions={champions}/>
+            <FinalDeck champions={champions}/>
+        </div>
     )
 }
