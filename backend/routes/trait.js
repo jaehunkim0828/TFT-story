0cc2063b3b7bff76a45797007ad2086be7a6f99d
JaeHun Kim <kkaa81@naver.com>
2022-01-27 23:31:03 +0900

    insert sugment  data

--- backend/routes/trait.js
+++ backend/routes/trait.js
@@ -1,16 +1,28 @@
 import express from "express";
 
 import { db } from '../db.js';
+import { sugmented } from "../sugmented.js";
 
 const traitRouter = express.Router();
 
-traitRouter.route('/:id')
+// traitRouter.route('/:id')
     // .get(async (req, res, next) => {
     //     const { id } = req.params;
     //     await db.execute('SELECT traitId FROM champion_trait WHERE championId=?', [id])
     //     .then(result => res.status(200).send(result[0]))
     //     .catch(err => res.status(404).send({ message: 'not Found' }));
     // });
+traitRouter.route('/').post((req, res, next) => {
+    sugmented.map(async ({ name, des }) => {
+        const done = await db.execute('INSERT INTO sugmented (name, description) VALUES (?, ?)',[name, des]);
+
+        console.log(done);
+    })
+    res.send('done');
+})
+.get((req, res, next) => {
+    res.send('trait')
+})
 
 
 export default traitRouter;
