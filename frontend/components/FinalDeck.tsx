0a974b0b37201e95673ee4c89262f48d3e7d2019
JaeHun Kim <kkaa81@naver.com>
2022-02-01 10:47:21 +0900

    feat(function): change item info per changing member

--- frontend/components/FinalDeck.tsx
+++ frontend/components/FinalDeck.tsx
@@ -5,17 +5,34 @@ import Champions from "./Champions";
 import style from '../styles/makeCard.module.scss';
 import Table from "./Table";
 import TraitCount from "./TraitCount";
+import { useEffect, useState } from 'react';
+import { RootState } from '../store/reducers';
+import { useSelector } from 'react-redux';
+import FinalItems from './FinalItems';
 
-function FinalDeck({ champions }: any) {
+
+function FinalDeck({ champions, deckInfo, setDeckInfo }: any) {
+    const [isItem, setIsItem] = useState(false);
+
+    const { member } = useSelector((state: RootState) => state.numberOfChampReducer);
+
+    useEffect(() => {
+        if (member > 0) {
+            setIsItem(true);
+            return;
+        }
+        setIsItem(false);
+    }, [member])
 
     return (
         <DndProvider backend={HTML5Backend}>
-            <div className={style.container}>
+            <div className={style.finalContainer}>
                 <div className="table-trait">
                     <TraitCount />
-                    <Table />
+                    <Table deckInfo={deckInfo} setDeckInfo={setDeckInfo} setIsItem={setIsItem}/>
                 </div>
                 <Champions champions={champions}/>
+                {isItem && <FinalItems deckInfo={deckInfo} setDeckinfo={setIsItem}/>}
             </div>
         </DndProvider>
     );
