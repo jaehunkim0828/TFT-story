d070a531aead33914ee747a9dcb0adf3276f5665
JaeHun Kim <kkaa81@naver.com>
2022-02-01 10:45:23 +0900

    fix(function): add props

--- frontend/components/Table.tsx
+++ frontend/components/Table.tsx
@@ -4,7 +4,7 @@ import { useDrop } from 'react-dnd';
 import style from '../styles/table.module.scss';
 import DropChampion from './DropChampion';
 
-export default function Table() {
+export default function Table({ deckInfo, setDeckInfo }: any) {
 
     const dropsSection = Array(28).fill(0);
 
@@ -12,9 +12,9 @@ export default function Table() {
         <div className={style.table}>
             {dropsSection.map((_, i) => {
                 if (i < 7 || i > 13 && i < 21) {
-                    return <DropChampion key={i} styles={{ right: '5%'}} indexs={i}/>
+                    return <DropChampion deckInfo={deckInfo} setDeckInfo={setDeckInfo} key={i} styles={{ right: '5%'}} indexs={i}/>
                 }
-                return <DropChampion key={i} styles={{ left: '5%'}} indexs={i}/>
+                return <DropChampion deckInfo={deckInfo} setDeckInfo={setDeckInfo} key={i} styles={{ left: '5%'}} indexs={i}/>
             })}
 
             <div className={style.triangle}/>
