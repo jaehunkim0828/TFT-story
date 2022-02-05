cd6c327b94c16fe3e6223a4e39b171a953c6742c
JaeHun Kim <kkaa81@naver.com>
2022-02-05 21:31:32 +0900

    feat(main): count up when go to deckmake

--- frontend/pages/main.tsx
+++ frontend/pages/main.tsx
@@ -1,15 +1,23 @@
 import Link from "next/link";
 import CardList from "../components/CardList";
+import { useDispatch, useSelector } from "react-redux";
 
 import style from '../styles/main.module.scss';
+import { countUpDeckMake } from '../store/actions/saveDeck';
+
 
 export default function Main() {
+    const dispatch = useDispatch();
+
+    const goDeckMake = () => {
+        dispatch(countUpDeckMake());
+    }
+
     return (
-        // <Link href={'/makeCard'} ><a>덱 만들기</a></Link>
         <div className={style.container}>
             <div className={style.mainNav}>
                 <div>카드 리스트</div>
-                <Link href={'/makeCard'}><a className={style.makeCard}>카드 만들기</a></Link>
+                <Link href={'/makeCard'}><a onClick={goDeckMake} className={style.makeCard}>카드 만들기</a></Link>
             </div>
             <hr />
             <CardList />
