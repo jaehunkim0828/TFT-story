fe146b6e9c10a1aba9440d039431b9ea822454ae
JaeHun Kim <kkaa81@naver.com>
2022-02-08 19:46:55 +0900

    trait api

---
+++ backend/traits.js
@@ -0,0 +1,158 @@
+export const championTrait = [
+    { championId: 79, name: '드레이븐', traitName: '', traitId: 0 },
+{ championId: 80, name: '녹턴', traitName: '', traitId: 0 },
+{ championId: 83, name: '렉사이', traitName: '', traitId: 0 },
+{ championId: 84, name: '루시안', traitName: '', traitId: 0 },
+{ championId: 85, name: '레나타', traitName: '', traitId: 0 },
+{ championId: 87, name: '나르', traitName: '', traitId: 0 },
+{ championId: 90, name: '룰루', traitName: '', traitId: 0 },
+{ championId: 91, name: '다리우스', traitName: '', traitId: 0 },
+{ championId: 92, name: '레오나', traitName: '', traitId: 0 },
+{ championId: 93, name: '미스포츈', traitName: '', traitId: 0 },
+{ championId: 94, name: '말자하', traitName: '', traitId: 0 },
+{ championId: 95, name: '갱플랭크', traitName: '', traitId: 0 },
+{ championId: 96, name: '모르가나', traitName: '', traitId: 0 },
+{ championId: 97, name: '갈리오', traitName: '', traitId: 0 },
+{ championId: 98, name: '바이', traitName: '', traitId: 0 },
+{ championId: 99, name: '베이가', traitName: '', traitId: 0 },
+{ championId: 100, name: '벡스', traitName: '', traitId: 0 },
+{ championId: 101, name: '브라움', traitName: '', traitId: 0 },
+{ championId: 102, name: '브랜드', traitName: '', traitId: 0 },
+{ championId: 103, name: '블리츠크랭크', traitName: '', traitId: 0 },
+{ championId: 104, name: '빅토르', traitName: '', traitId: 0 },
+{ championId: 105, name: '세나', traitName: '', traitId: 0 },
+{ championId: 106, name: '세라핀', traitName: '', traitId: 0 },
+{ championId: 107, name: '뽀삐', traitName: '', traitId: 0 },
+{ championId: 108, name: '세주아니', traitName: '', traitId: 0 },
+{ championId: 109, name: '스웨인', traitName: '', traitId: 0 },
+{ championId: 110, name: '시비르', traitName: '', traitId: 0 },
+{ championId: 111, name: '신드라', traitName: '', traitId: 0 },
+{ championId: 112, name: '신지드', traitName: '', traitId: 0 },
+{ championId: 113, name: '실코', traitName: '', traitId: 0 },
+{ championId: 114, name: '아리', traitName: '', traitId: 0 },
+{ championId: 115, name: '애쉬', traitName: '', traitId: 0 },
+{ championId: 116, name: '알리스타', traitName: '', traitId: 0 },
+{ championId: 117, name: '에코', traitName: '', traitId: 0 },
+{ championId: 118, name: '오리아나', traitName: '', traitId: 0 },
+{ championId: 119, name: '워윅', traitName: '', traitId: 0 },
+{ championId: 120, name: '이렐리아', traitName: '', traitId: 0 },
+{ championId: 121, name: '이즈리얼', traitName: '', traitId: 0 },
+{ championId: 122, name: '일라오이', traitName: '', traitId: 0 },
+{ championId: 123, name: '자르반4세', traitName: '', traitId: 0 },
+{ championId: 124, name: '자이라', traitName: '', traitId: 0 },
+{ championId: 125, name: '자크', traitName: '', traitId: 0 },
+{ championId: 126, name: '제리', traitName: '', traitId: 0 },
+{ championId: 127, name: '제이스', traitName: '', traitId: 0 },
+{ championId: 128, name: '직스', traitName: '', traitId: 0 },
+{ championId: 129, name: '진', traitName: '', traitId: 0 },
+{ championId: 130, name: '질리언', traitName: '', traitId: 0 },
+{ championId: 131, name: '징크스', traitName: '', traitId: 0 },
+{ championId: 132, name: '초가스', traitName: '', traitId: 0 },
+{ championId: 133, name: '카밀', traitName: '', traitId: 0 },
+{ championId: 134, name: '카사딘', traitName: '', traitId: 0 },
+{ championId: 135, name: '카이사', traitName: '', traitId: 0 },
+{ championId: 136, name: '카직스', traitName: '', traitId: 0 },
+{ championId: 137, name: '케이틀린', traitName: '', traitId: 0 },
+{ championId: 138, name: '코르키', traitName: '', traitId: 0 },
+{ championId: 139, name: '퀸', traitName: '', traitId: 0 },
+{ championId: 140, name: '탈론', traitName: '', traitId: 0 },
+{ championId: 141, name: '탐켄치', traitName: '', traitId: 0 },
+{ championId: 142, name: '트린다미어', traitName: '', traitId: 0 },
+{ championId: 143, name: '트위치', traitName: '', traitId: 0 },
+]
+
+
+export const chTr = [
+{ trait: 29, name: '경호대', champions: [103, 91, 101, 97, 92, 107]}, //6
+{ trait: 30, name: '난동꾼', champions: [98, 141, 83, 132, 108, 122, 125]}, //7 13
+{ trait: 31, name: '변형술사', champions: [127]}, // 1 14
+{ trait: 32, name: '저격수', champions: [115, 129, 126, 93, 137]}, // 5 19
+{ trait: 33, name: '비전마법사', champions: [128, 94, 109, 114, 102, 100, 104]}, // 7 26
+{ trait: 34, name: '도전자', champions: [135, 133, 79, 139, 119, 142]}, // 6 32
+{ trait: 35, name: '암살자', champions: [117, 136, 80, 140, 143]}, // 5 37
+{ trait: 36, name: '쌍발총', champions: [131, 84, 138, 95]}, // 4 41
+{ trait: 37, name: '강화술사', champions: [96, 105, 118, 90]}, // 4 45
+{ trait: 38, name: '거신', champions: [132, 116, 97]}, // 3 48
+{ trait: 39, name: '타격대', champions: [120, 83, 123, 110, 87]}, // 5 53
+{ trait: 40, name: '학자', champions: [134, 124, 111, 85, 113]}, // 5 58
+{ trait: 41, name: '혁신가', champions: [121, 117, 106, 130, 127, 112]}, // 6 64
+{ trait: 42, name: '경쟁자', champions: [98, 131]}, // 2 66
+{ trait: 43, name: '고물상', champions: [103, 128, 131, 117, 120, 121]}, // 6 72
+{ trait: 44, name: '대식가', champions: [141]}, // 1 73
+{ trait: 45, name: '돌연변이', champions: [83, 132,	135, 94, 136, 134]}, // 6 79
+{ trait: 46, name: '마법공학', champions: [116,	108, 109, 84, 80, 123, 110]}, // 7 86
+{ trait: 47, name: '범죄조직', champions: [96, 91, 101, 114, 115,124]}, // 6 92
+{ trait: 48, name: '사교계', champions: [105, 97, 87, 106]}, // 4 96
+{ trait: 49, name: '시계태엽', champions: [118, 133, 129, 130]}, // 4 100
+{ trait: 50, name: '연미복', champions: [92, 79, 102, 140, 126, 111	]}, // 6 106
+{ trait: 51, name: '요들', champions: [90, 107, 128, 100, 138, 87]}, // 6 112
+{ trait: 52, name: '요들군주', champions: [99]}, // 1 113
+{ trait: 53, name: '용병', champions: [122, 141, 139, 95, 93 ]}, // 5 118
+{ trait: 54, name: '집행자', champions: [108, 98, 127, 137]}, // 4 122
+{ trait: 55, name: '화학공학', champions: [125, 119, 142, 104, 143, 85,	112]}, // 7 129
+{ trait: 56, name: '흑막', champions: [113]}, // 1 130
+]
+
+
+
+    // 강화술사
+    // 거신
+    // 경호대
+    // 난동꾼
+    // 도전자
+    // 변형술사
+    // 비전마법사
+    // 쌍발총
+    // 암살자
+    // 저격수
+    // 타격대
+    // 학자
+    // 혁신가
+    // 경쟁자
+    // 고물상
+    // 대식가 = 탐 켄치									
+    // 돌연변이 = 132		83 132	카이사		말자하		카직스		렉사이	134	
+    // 마법공학 = 116		세주아니			스웨인	루시안	녹턴		자르반 4세 110		
+    // 범죄조직 = 모르가나		다리우스 브라움				아리			애쉬		자이라	
+    // 사교계 = 세나	97	갈리오								나르		세라핀
+    // 시계태엽 = 118				카밀					진			질리언
+    // 연미복 = 레오나		드레이븐		브랜드		탈론	제리		신드라	
+    // 요들 = 90		뽀삐				직스 벡스	코르키			나르		
+    // 요들군주 = 베이가
+    // 용병 = 일라오이 탐 켄치	퀸			갱플랭크		미스 포츈			
+    // 집행자 = 세주아니 바이		제이스				케이틀린			제이스
+    // 화학공학 = 자크	워윅 트린다미어		빅토르		트위치			레나타 글라스크	신지드
+    // 흑막
+    // 실코	
+
+export const trait =[
+{ name: '강화술사',  image: '/images/traits/강화술사.png' },
+{ name: '거신',  image: '/images/traits/거신.png' },
+{ name: '경호대',  image: '/images/traits/경호대.png' },
+{ name: '난동꾼',  image: '/images/traits/난동꾼.png' },
+{ name: '도전자',  image: '/images/traits/도전자.png' },
+{ name: '변형술사',  image: '/images/traits/변형술사.png' },
+{ name: '비전마법사',  image: '/images/traits/비전마법사.png' },
+{ name: '쌍발총',  image: '/images/traits/쌍발총.png' },
+{ name: '암살자',  image: '/images/traits/암살자.png' },
+{ name: '저격수',  image: '/images/traits/저격수.png' },
+{ name: '타격대',  image: '/images/traits/타격대.png' },
+{ name: '학자',  image: '/images/traits/학자.png' },
+{ name: '혁신가',  image: '/images/traits/혁신가.png' },
+{ name: '경쟁자',  image: '/images/traits/경쟁자.png' },
+{ name: '고물상',  image: '/images/traits/고물상.png' },
+{ name: '대식가',  image: '/images/traits/대식가.png' },
+{ name: '돌연변이',  image: '/images/traits/돌연변이.png' },
+{ name: '마법공학',  image: '/images/traits/마법공학.png' },
+{ name: '범죄조직',  image: '/images/traits/범죄조직.png' },
+{ name: '사교계',  image: '/images/traits/사교계.png' },
+{ name: '시계태엽',  image: '/images/traits/시계태엽.png' },
+{ name: '연미복',  image: '/images/traits/연미복.png' },
+{ name: '요들',  image: '/images/traits/요들.png' },
+{ name: '요들군주',  image: '/images/traits/요들군주.png' },
+{ name: '용병',  image: '/images/traits/용병.png' },
+{ name: '집행자',  image: '/images/traits/집행자.png' },
+{ name: '화학공학',  image: '/images/traits/화학공학.png' },
+{ name: '흑막',  image: '/images/traits/흑막.png' },
+{ name: '실코',  image: '/images/traits/실코.png' },
+]
