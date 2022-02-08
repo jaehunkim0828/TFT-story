6086e689fe831f1b3a16079dcbf3e8ba6d64cee2
JaeHun Kim <kkaa81@naver.com>
2022-02-08 19:51:39 +0900

    feat(card): update card info

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 
 import style from '../styles/cardList.module.scss';
 import { CardItem } from '../type';
+import httpImage from '../server';
 
 export default function Card({i, deck}: CardItem) {
     const router = useRouter();
@@ -19,13 +20,17 @@ export default function Card({i, deck}: CardItem) {
             key={i}
             onClick={() => goCardInfo()}
         >
-            <img className={style.cardImage}src={deck.image} alt='deck-image'/>
+            <img className={style.cardImage}src={httpImage(deck.image)} alt='deck-image'/>
             <div>
                 <div className={style.name}>{deck.name}</div>
                 <div className={style.trait}>
-                    {deck.trait.split(' > ').map((item, i) => <div key={i}>#{item}</div>)}
+                    {deck.trait.split(' > ').map((item, i) => {
+                        if (i < 3) {
+                            return (<div key={i}>#{item}</div>);
+                        }
+                    })}
                 </div>
-                <div>설명: {deck.description}</div>
+                <div>설명: {`${deck.description.substring(0, 31)}...`}</div>
             </div>
         </div>
     )
