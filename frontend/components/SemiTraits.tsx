29245f43d013500e3494779ca517144a4a8a7121
JaeHun Kim <kkaa81@naver.com>
2022-02-12 14:03:51 +0900

    create traits design

--- frontend/components/SemiTraits.tsx
+++ frontend/components/SemiTraits.tsx
@@ -1,19 +1,34 @@
+/* eslint-disable @next/next/no-img-element */
 import axios from "axios";
 import { useEffect, useState } from "react"
 
 import { http } from '../server';
+import style from '../styles/cardInfo.module.scss';
 
 export default function SemiTraits({ traits }: { traits: string }) {
     const [traitsData, setTraits] = useState<any[]>([]);
 
     useEffect(() => {
+        const traitresult: any[] = [];
         traits.split(' > ').map( async(trait: string) => {
             const traitArr = trait.split(' ');
-            const image = await axios.get(http + `/trait/${traitArr[0]}`);
-            console.log(image.data.id);
+            if (traitArr[0]) {
+                traitresult.push({ trName: traitArr[0], trImage: http + `/images/traits/${traitArr[0]}.svg`, count: traitArr[1] });
+            };
         } )
-    }, [])
+        setTraits(traitresult);
+    }, [traits])
     return (
-        <div></div>
+        <div className={style.traits}>
+            {
+                traitsData.map((tr: any, i: number) => (
+                    <div className={ style.traitRow } key={i}>
+                        <img className={ style.traitImage } src={tr.trImage} alt='trait'/>
+                        <div>{tr.trName}</div>
+                        <div className={ style.count }>{tr.count}</div>
+                    </div>
+                ))    
+            }
+        </div>
     )
 }
