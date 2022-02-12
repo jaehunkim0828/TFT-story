2d4b60665f5cdf0653c59d075fc6370298c08e07
JaeHun Kim <kkaa81@naver.com>
2022-02-12 14:05:56 +0900

    add season 6.5

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -9,6 +9,7 @@ export default function NavBar() {
                 <Link href='/'><a className={style.mainLogo}>TFT Story</a></Link>
                 <span className={style.subLogo}>원하는 덱을 만들어보세요.</span>
             </div>
+            <div className={style.season}>시즌 6.5</div>
         </div>
     )
 }
