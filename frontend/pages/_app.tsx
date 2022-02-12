9078ef6601943f4cbe673f29053c242691ee0bf3
JaeHun Kim <kkaa81@naver.com>
2022-02-12 14:02:26 +0900

    add font

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,19 +1,21 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { AppProps } from "next/dist/shared/lib/router/router";
 import { useRouter } from "next/router";
+import Head from 'next/head';
 
 import NavBar from "../components/NavBar";
 import '../styles/globals.css';
 import { wrapper } from '../store';
-import { useEffect } from "react";
 
 function App({ Component, PageProp }: AppProps) {
     const router = useRouter();
 
 return (
         <div>
-            <head>
+            <Head>
                 <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet"></link>
-            </head>
+                <link href="https://fonts.googleapis.com/css2?family=Jua&family=Nanum+Gothic+Coding&display=swap" rel="stylesheet"></link>
+            </Head>
             {router.pathname !== '/' ? <NavBar /> : <div></div>}
             <Component {...PageProp}/>
         </div>
