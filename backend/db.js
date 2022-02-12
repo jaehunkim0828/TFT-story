f0ac20b1dea51306656a2fea8a765c6f89bf6437
JaeHun Kim <kkaa81@naver.com>
2022-02-12 21:23:43 +0900

    change db data

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = mysql.createPool({
-    host: '15.165.15.185',
-    user: 'root',
-    database: 'tft_db',
-    password: 'gkfajsl132!'
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
 })
 
 export const db = pool.promise();
