635afa787c375f2bb3e426d9d2a9145183d9c1a1
JaeHun Kim <kkaa81@naver.com>
2022-02-26 00:04:32 +0900

    update item api

--- backend/routes/item.js
+++ backend/routes/item.js
@@ -4,12 +4,18 @@ import { db } from '../db.js';
 const itemRouter = express.Router();
 
 itemRouter.route('/').get( async (req, res, next) => {
-    const items = await db.execute('SELECT name, image, id FROM items');
+    const items = await db.execute('SELECT * FROM items');
     res.send(items[0]);
 })
 itemRouter.route('/:id').get( async (req, res, next) => {
     const { id } = req.params;
     const image = await db.execute('SELECT image FROM items WHERE id=?', [id]);
+    res.status(200).send(image[0][0]);
+})
+
+itemRouter.route('/com/:id').get(async (req, res, next) => {
+    const { id } = req.params;
+    const image = await db.execute('SELECT image FROM combination WHERE id=?', [id]);
     res.status(200).send(image[0][0]);
 })
 
