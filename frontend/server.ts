21ed8ad3d643a9dce58d9526b427ebead18fc1f3
JaeHun Kim <kkaa81@naver.com>
2022-02-26 00:05:23 +0900

    update

--- frontend/server.ts
+++ frontend/server.ts
@@ -1,5 +1,5 @@
 export default function httpServer(image: string) {
-    return `http://15.165.15.185:8080${image}`;
+    return `http://localhost:8080${image}`;
 }
 
-export const http = 'http://15.165.15.185:8080';
+export const http = 'http://localhost:8080';
