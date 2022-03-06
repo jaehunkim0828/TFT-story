4383130740705062fdbcc467fd31946682ed6d1e
JaeHun Kim <kkaa81@naver.com>
2022-03-07 00:40:28 +0900

    local to 15.165.15.185

--- backend/config.js
+++ backend/config.js
@@ -1,13 +1,12 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-    apiKey: process.env.API_KEY,
-    db: {
-        host: '15.165.15.185',
-        user: 'root',
-        // datebase: '',
-        // password: ,
-
-    }
-}
+  apiKey: process.env.API_KEY,
+  db: {
+    host: "15.165.15.185",
+    user: "root",
+    // datebase: '',
+    // password: ,
+  },
+};
