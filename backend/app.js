82c43ad7349a7836612c2c527e60e8e891b3fc4a
JaeHun Kim <kkaa81@naver.com>
2022-03-07 00:40:22 +0900

    local to 15.165.15.185

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,27 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import championRouter from './routes/champion.js';
-import cardRouter from './routes/card.js';
-import traitRouter from './routes/trait.js';
-import augmentedRouter from './routes/augmented.js';
-import itemRouter from './routes/item.js';
-import path from 'path';
+import championRouter from "./routes/champion.js";
+import cardRouter from "./routes/card.js";
+import traitRouter from "./routes/trait.js";
+import augmentedRouter from "./routes/augmented.js";
+import itemRouter from "./routes/item.js";
+import path from "path";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(path.resolve(), 'public')));
+app.use(express.static(path.join(path.resolve(), "public")));
 
-app.use('/champion',championRouter);
-app.use('/card', cardRouter);
-app.use('/trait', traitRouter);
-app.use('/augmented', augmentedRouter);
-app.use('/item', itemRouter);
-app.use('/images', express.static('images'));
-app.get('/', (req, res, next) => {
-    res.send('hello')
-})
+app.use("/champion", championRouter);
+app.use("/card", cardRouter);
+app.use("/trait", traitRouter);
+app.use("/augmented", augmentedRouter);
+app.use("/item", itemRouter);
+app.use("/images", express.static("images"));
+app.get("/", (req, res, next) => {
+  res.send("hello");
+});
 
-app.listen(8080, () => console.log(`running 15.165.15.185:${8080}`))
+app.listen(8080, () => console.log(`running 15.165.15.185:${8080}`));
--- frontend/server.ts
+++ frontend/server.ts
@@ -1,5 +1,5 @@
 export default function httpImage(image: string) {
-    return `http://15.165.15.185:8080${image}`;
+  return `http://15.165.15.185:8080${image}`;
 }
 
-export const http = 'http://15.165.15.185:8080';
+export const http = "http://15.165.15.185:8080";
